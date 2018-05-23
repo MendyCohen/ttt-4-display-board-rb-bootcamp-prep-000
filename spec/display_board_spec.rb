b3e35cb16687ee9ab2dd0a508a97571b305c9c27
Mendy <mendybeltcohen@gmail.com>
2018-05-23 19:55:51 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -163,8 +163,8 @@ describe "#display_board in 'lib/display_board.rb" do
       # Then edit the following lines to represent a board entirely filled with X.
       # Remember, every space and every character is important.
 
-       *** Edit the lines below ***
-       *** Uncomment the lines below ***
+       #*** Edit the lines below ***
+       #*** Uncomment the lines below ***
        expect(rows[0]).to eq(" X | X | X ")
        expect(rows[1]).to eq("-----------")
        expect(rows[2]).to eq(" X | X | X ")
