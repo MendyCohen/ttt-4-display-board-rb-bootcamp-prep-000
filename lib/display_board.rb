7438521c77b328f33150b72117fc3f2c9c4b91c2
Mendy <mendybeltcohen@gmail.com>
2018-05-23 18:37:26 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,14 +3,14 @@
 
 
 
-def display_board(board)
-  puts "#{board[0]}   ""|""#{board[1]]   ""|""#{board[2]   "
-  puts "-----------"
-  puts "#{board[3]}   ""|""#{board[4]}   ""|""#{board[5]}   "   
-  puts "-----------"
-  puts "#{board[6]}   ""|""#{board[7]}   " "|""#{board[8]}   "
-end
-
-move = ["", "", "", "", "", "", "", "", ""]
-display_board(move)
+#def display_board(board)
+#  puts "#{board[0]}   ""|""#{board[1]]   ""|""#{board[2]   "
+#  puts "-----------"
+#  puts "#{board[3]}   ""|""#{board[4]}   ""|""#{board[5]}   "   
+#  puts "-----------"
+#  puts "#{board[6]}   ""|""#{board[7]}   " "|""#{board[8]}   "
+#end
+#
+#move = ["", "", "", "", "", "", "", "", ""]
+#display_board(move)
 
