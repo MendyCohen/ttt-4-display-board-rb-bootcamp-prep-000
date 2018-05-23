6117d4ad82dc685e9a91b7dbbdc8ea52c04d2f5d
Mendy <mendybeltcohen@gmail.com>
2018-05-23 19:30:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,27 +3,27 @@
 
 
 
-#def display_board(board)
-#  puts "#{board[0]}   ""|""#{board[1]]   ""|""#{board[2]   "
-#  puts "-----------"
-#  puts "#{board[3]}   ""|""#{board[4]}   ""|""#{board[5]}   "   
-#  puts "-----------"
-#  puts "#{board[6]}   ""|""#{board[7]}   " "|""#{board[8]}   "
-#end
-#
-#board = ["", "", "", "", "", "", "", "", ""]
-#display_board(board)
+def display_board(board)
+  puts "#{board[0]}   ""|""#{board[1]]   ""|""#{board[2]   "
+  puts "-----------"
+  puts "#{board[3]}   ""|""#{board[4]}   ""|""#{board[5]}   "   
+  puts "-----------"
+  puts "#{board[6]}   ""|""#{board[7]}   " "|""#{board[8]}   "
+end
+
+board = ["", "", "", "", "", "", "", "", ""]
+display_board(board[4])
 
  
  
-def display_board(board) 
-  puts "   ""|""   ""|""   "
-  puts "-----------"
-  puts "   ""|""   ""|""   "   
-  puts "-----------"
-  puts "   ""|""   " "|""   "
- end
- 
-board = ["", "", "", "", "X", "", "", "", ""]
-puts display_board("#{board[4]}")
+#def display_board(board) 
+#  puts "   ""|""   ""|""   "
+#  puts "-----------"
+#  puts "   ""|""   ""|""   "   
+#  puts "-----------"
+#  puts "   ""|""   " "|""   "
+# end
+# 
+#board = ["", "", "", "", "X", "", "", "", ""]
+#puts display_board("#{board[4]}")
  
