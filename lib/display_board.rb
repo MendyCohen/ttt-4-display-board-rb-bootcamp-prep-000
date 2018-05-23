2e85750b1c36e08abc5f747562ae531c5a869325
Mendy <mendybeltcohen@gmail.com>
2018-05-23 19:46:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,7 +11,7 @@ def display_board(board)
   puts " #{board[6]}  ""|"" #{board[7]} ""|"" #{board[8]} "
 end
 
-board = ["X", "X", "X", "X", "X", "X", "X", "", "X"]
+board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
 puts display_board(board)
 
  
